e1cc0c4d5e69b66d3fc7d11ceb609cdeb64c1a51
Hieu Nguyen <hieutk5@gmail.com>
2024-04-15 00:04:36 +0700

    Fix css value

--- src/components/main/banner/index.tsx
+++ src/components/main/banner/index.tsx
@@ -54,25 +54,33 @@ export default function Banner() {
               <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                   <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": days }}></span>
+                    <span
+                      style={{ "--value": days } as React.CSSProperties}
+                    ></span>
                   </span>
                   days
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                   <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": hours }}></span>
+                    <span
+                      style={{ "--value": hours } as React.CSSProperties}
+                    ></span>
                   </span>
                   hours
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                   <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": minutes }}></span>
+                    <span
+                      style={{ "--value": minutes } as React.CSSProperties}
+                    ></span>
                   </span>
                   min
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                   <span className="countdown font-mono text-5xl">
-                    <span style={{ "--value": seconds }}></span>
+                    <span
+                      style={{ "--value": seconds } as React.CSSProperties}
+                    ></span>
                   </span>
                   sec
                 </div>
